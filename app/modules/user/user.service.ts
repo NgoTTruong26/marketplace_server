ead5da8dbc1c01451bb7594b13ab8e2fbb44f9c2
unknown <tiennhannaruto@gmail.com>
2024-07-16 11:46:39 +0700

    feat: update profile

--- app/modules/user/user.service.ts
+++ app/modules/user/user.service.ts
@@ -1,16 +1,26 @@
+import DatabaseException from '#exceptions/database_exception'
 import Profile from '#models/profile'
 import { UpdateProfileDTO } from './dto/update_profile.dto.js'
 
 export default class UsersService {
-  async getProfile(userId: number) {
-    const profile = await Profile.findByOrFail('user_id', userId)
-
-    return profile
+  async getProfile(userId: number): Promise<Profile> {
+    try {
+      const profile = await Profile.findByOrFail('user_id', userId)
+      return profile
+    } catch (error) {
+      throw new DatabaseException(error.message)
+    }
   }
 
-  async updateProfile(userId: number, data: UpdateProfileDTO) {
-    console.log('User Id', userId)
+  async updateProfile(userId: number, data: UpdateProfileDTO): Promise<Profile> {
+    try {
+      const profile = await Profile.findByOrFail('user_id', userId)
 
-    console.log('Updating profile', data)
+      const newProfile = await profile.merge(data).save()
+
+      return newProfile
+    } catch (error) {
+      throw new DatabaseException(error.message)
+    }
   }
 }
